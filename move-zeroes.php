5beded3ac9ddaba39209031a83bf4aa7a22158dc
jmatah <msolutions.co.in@gmail.com>
2022-02-23 16:29:36 +0530

    Feb 23

---
+++ move-zeroes.php
@@ -0,0 +1,25 @@
+<?php
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return NULL
+     */
+    function moveZeroes(&$nums) {
+        $zero = array_count_values($nums);
+        if( $zero[0] > 0 ){
+        foreach( range(1, $zero[0]) as $r ) {
+            array_splice( $nums, array_search( 0, $nums ), 1 );
+            array_push( $nums, 0 );
+        }}
+    }
+}
+
+
+$nums = [0,1,0,3,12];
+$nums = [0, 0,0,0,0,120,0,34,45,56,67];
+print_r( $nums);
+
+$sol = new Solution();
+$sol->MoveZeroes($nums);
+print_r( $nums);
---
+++ move-zeroes.py
@@ -0,0 +1,19 @@
+class Solution:
+    def moveZeroes(self, nums: list[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        zeroes = nums.count(0)
+        if zeroes > 0:
+            for i in range(zeroes):
+                del nums[nums.index(0)]
+                nums.append(0)
+        print(nums)
+
+nums = [0,1,0,3,12]
+#nums = [0, 0,0,0,0,120,0,34,45,56,67]
+print(nums)
+
+sol = Solution()
+sol.moveZeroes(nums)
+print(nums)
