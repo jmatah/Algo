537b36b92faf96846ce1f0ac0baf46d6ef1c1f8c
jmatah <msolutions.co.in@gmail.com>
2022-03-08 14:39:07 +0530

    8 March

--- permutation-in-strings.py
+++ permutation-in-strings.py
@@ -1,10 +1,26 @@
+import collections 
+
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        for left in range(len(s1)):
-            if( s2.find(s1[left]) == -1 ):
-                return False
-
-        return True
+        """
+        :type s1: str
+        :type s2: str
+        :rtype: bool
+        """
+        counts = collections.Counter(s1)
+        l = len(s1)
+        for i in range(len(s2)):
+            if counts[s2[i]] > 0:
+                l -= 1
+            counts[s2[i]] -= 1
+            if l == 0:
+                return True
+            start = i + 1 - len(s1)
+            if start >= 0:
+                counts[s2[start]] += 1
+                if counts[s2[start]] > 0:
+                    l += 1
+        return False
 
 s1 = "ab"
 s2 = "eidboaoo"
